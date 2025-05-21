041004f225f73fce8f8c38c40483cc1b0c0650d4
Ylenia <yleniamighela@gmail.com>
2025-05-21 11:01:29 +0200

    fix code

--- src/main.ts
+++ src/main.ts
@@ -116,7 +116,7 @@ type Team = {
   nome: string,
   progettoAttuale: string | null,
   budget: number,
-  membri: [ProjectManager, ...Developer[]]
+  membri: [ProjectManager, Developer, ...Developer[]]
 }
 
 const team: Team = {
